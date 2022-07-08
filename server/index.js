ad1007af4f2298bade9402c40d8e9e52d30eb8ec
Zhao Yuan (James) Li <zyli2@illinois.edu>
2022-07-08 14:32:06 -0500

    set up dependencies and connection to MongoDB

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// credentials will be secured later
+const CONNECTION_URL = 'mongodb+srv://zyli2:LiDuckIOB9010@cluster0.atkb3ix.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
