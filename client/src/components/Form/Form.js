c0f129c7a4626bb1b5d7adcc2cfaa44d5746e923
Zhao Yuan (James) Li <zyli2@illinois.edu>
2022-07-12 00:46:14 -0500

    finished add form component

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,11 +1,43 @@
-import React from 'react';
+// this is the code for creating a "memory"/form on the website
 
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import FileBase from 'react-file-base64';
 import useStyles from './styles';
 
 const Form = () => {
+    const [postData, setPostData] = useState({
+        // creator/wtv: '' means it's an empty string
+        creator: '', title: '', message: '', tags: '', selectedFile: ''});
     const classes = useStyles();
+
+    const handleSubmit = () => {
+
+    }
+
+    const clear = () => {
+        
+    }
+
     return (
-        <h1>FORM</h1>
+        // paper is a white background
+        // VALUE here means that the whole data from the poast is gonna be stored in the postData object
+                // and value is gonna equal to postData.creator
+        // onChange allows us to set each of the different textfields without overriding once another
+            // this means setting fields every time but the last property is changed so
+            // all the textfield will be fulfilled
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                <Typography variant="h6">Creating a Memory</Typography>
+                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
+                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}/>
+                <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}/>
+                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })}/>
+                <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64 })} /></div>
+                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+                <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
+            </form>
+        </Paper>
     );
 }
 
