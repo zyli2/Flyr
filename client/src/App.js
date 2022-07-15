f1fc029ddcdc5e04b9b41c1114963c13b76eec92
James Li <zyli2@illinois.edu>
2022-07-15 14:40:52 -0700

    completed sign up page with sign in and sign up

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Container } from '@material-ui/core';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 
-import { getPosts } from './actions/posts';
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import memories from './images/memories.png';
-import useStyles from './styles';
-
-const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
-
-    return (
-        <Container maxidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60"/>
-            </AppBar>
-            <Grow in>
-                <Container>
-                <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId}/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
+const App = () => (
+    <BrowserRouter>
+        <Container maxWidth="lg">
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home/>} exact />
+                <Route path="/auth" element={<Auth/>} exact />
+            </Routes>
         </Container>
-    );
-}
+    </BrowserRouter>
+);
 
 export default App;
